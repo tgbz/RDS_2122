1b1aa95f15820bb73b6f3d73558af84ff6b887f7
tgbz <tgomesbz@gmail.com>
2022-06-18 16:58:05 +0100

    test

--- TP2/topo.py
+++ TP2/topo.py
@@ -11,31 +11,6 @@ def topo():
     r2 = net.addSwitch('r2')
     r3 = net.addSwitch('r3')
     
-    r1.setMac('00:00:00:00:00:01','r1-eth1')
-    r1.setMac('00:00:00:00:00:02','r1-eth2')
-    r1.setMac('00:00:00:00:00:03','r1-eth3')      
-    
-    r2.setMac('00:00:00:00:00:04','r2-eth1')
-    r2.setMac('00:00:00:00:00:05','r2-eth2')
-    r2.setMac('00:00:00:00:00:06','r2-eth3')
-    
-    r3.setMac('00:00:00:00:00:07','r3-eth1')
-    r3.setMac('00:00:00:00:00:08','r3-eth2')
-    r3.setMac('00:00:00:00:00:09','r3-eth3')
-    
-    s1.setMac('00:00:00:00:02:01','s1-eth1')
-    s1.setMac('00:00:00:00:02:02','s1-eth2')
-    s1.setMac('00:00:00:00:02:03','s1-eth3')
-    
-    s2.setMac('00:00:00:00:02:04','s2-eth1')
-    s2.setMac('00:00:00:00:02:05','s2-eth2')
-    s2.setMac('00:00:00:00:02:06','s2-eth3')
-    
-    s3.setMac('00:00:00:00:02:07','s3-eth1')
-    s3.setMac('00:00:00:00:02:08','s3-eth2')
-    s3.setMac('00:00:00:00:02:09','s3-eth3')
-    
-
     
     s1 = net.addSwitch('s1')
     s2 = net.addSwitch('s2')
@@ -47,23 +22,23 @@ def topo():
     
     #Configuracao dos hosts
     #R&D Division
-    web = net.addHost('web', ip='10.0.1.2/24', mac='00:00:00:00:01:01', defaultRoute = 'via 10.0.1.1')
-    h1 = net.addHost('h1', ip='10.0.1.3/24', mac='00:00:00:00:01:02', defaultRoute = 'via 10.0.1.1')
-    h2 = net.addHost('h2', ip='10.0.1.4/24', mac='00:00:00:00:01:03', defaultRoute = 'via 10.0.1.1')
+    h1 = net.addHost('h1', ip='10.0.1.2/24', mac='00:00:00:00:01:01', defaultRoute = 'via 10.0.1.1')
+    h2 = net.addHost('h2', ip='10.0.1.3/24', mac='00:00:00:00:01:02', defaultRoute = 'via 10.0.1.1')
+    h3 = net.addHost('h3', ip='10.0.1.4/24', mac='00:00:00:00:01:03', defaultRoute = 'via 10.0.1.1')
    
     #Client Support Division
-    h3 = net.addHost('h3', ip='10.0.2.2/24', mac='00:00:00:00:01:04', defaultRoute = 'via 10.0.2.1')
-    h4 = net.addHost('h4', ip='10.0.2.3/24', mac='00:00:00:00:01:05', defaultRoute = 'via 10.0.2.1')
-    h5 = net.addHost('h5', ip='10.0.2.4/24', mac='00:00:00:00:01:06', defaultRoute = 'via 10.0.2.1')
+    h4 = net.addHost('h4', ip='10.0.2.2/24', mac='00:00:00:00:01:04', defaultRoute = 'via 10.0.2.1')
+    h5 = net.addHost('h5', ip='10.0.2.3/24', mac='00:00:00:00:01:05', defaultRoute = 'via 10.0.2.1')
+    h6 = net.addHost('h6', ip='10.0.2.4/24', mac='00:00:00:00:01:06', defaultRoute = 'via 10.0.2.1')
     
     #Executive Division
-    h6= net.addHost('h7', ip='10.0.3.2/24', mac='00:00:00:00:01:07', defaultRoute = 'via 10.0.3.1')
-    h7 = net.addHost('h8', ip='10.0.3.3/24', mac='00:00:00:00:01:08', defaultRoute = 'via 10.0.3.1')
-    h8 = net.addHost('h9', ip='10.0.3.4/24', mac='00:00:00:00:01:09', defaultRoute = 'via 10.0.3.1')
+    h7= net.addHost('h7', ip='10.0.3.2/24', mac='00:00:00:00:01:07', defaultRoute = 'via 10.0.3.1')
+    h8 = net.addHost('h8', ip='10.0.3.3/24', mac='00:00:00:00:01:08', defaultRoute = 'via 10.0.3.1')
+    h9 = net.addHost('h9', ip='10.0.3.4/24', mac='00:00:00:00:01:09', defaultRoute = 'via 10.0.3.1')
     
     
     #Execute webServer on web host
-    web.cmd('web python -m http.server 5555 &')
+    #web.cmd('web python -m http.server 5555 &')
     
     #links intra-Routers
     net.addLink(r1, r2)
@@ -72,21 +47,21 @@ def topo():
     
     #R&D Division - Network A
     net.addLink(r1, s1)
-    net.addLink(s1, web)
-    net.addLink(s1,h1)
-    net.addLink(s1,h2)
+    net.addLink(s1, h1)
+    net.addLink(s1, h2)
+    net.addLink(s1, h3)
     
     #Client Support Division - Network B
     net.addLink(r2, s2)
-    net.addLink(s2, h3)
     net.addLink(s2, h4)
     net.addLink(s2, h5)
+    net.addLink(s2, h6)
     
     #Executive Division - Network C
     net.addLink(r3, s3)
-    net.addLink(s3, h6)
     net.addLink(s3, h7)
     net.addLink(s3, h8)
+    net.addLink(s3, h9)
         
         
     #addr intra-routers
